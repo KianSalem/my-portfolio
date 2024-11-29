01728d7d36ef2a4f591059bcea39f46dffbf7b05
KianSalem <salemkian1@gmail.com>
2024-11-29 06:21:18 -0600

    PlsWork

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import React, { useState } from 'react';
 import ThemeContext from './ThemeContext';
 
 function App() {
